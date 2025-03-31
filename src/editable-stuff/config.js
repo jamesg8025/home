02650c280766a7ba1f6146334f5924850d383f16
James Gonzalez <jamesg8025@gmail.com>
2025-03-31 03:53:27 -0500

    Convert rest of images to png

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -51,7 +51,7 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/DSCF5910.JPG"), // TODO: Change to your image
+  imageLink: require("../editable-stuff/DSCF5910.png"), // TODO: Change to your image
   //profilePictureLink: require("../editable-stuff/hashirshoaeb.png"), // TODO: Change to your image
   imageSize: 375,
   message:
@@ -80,12 +80,12 @@ const leadership = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
   images: [
     { 
-      img: require("../editable-stuff/DSCF5910.JPG"), 
+      img: require("../editable-stuff/DSCF5910.png"), 
       label: "First slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
     { 
-      img: require("../editable-stuff/DSCF5910.JPG"), 
+      img: require("../editable-stuff/DSCF5910.png"), 
       label: "Second slide label", 
       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
     },
